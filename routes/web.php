d4f83f49b12945442daf68311e4cc2fbeefcd3da
Huson Chiu <cchangive@gmail.com>
2025-01-03 15:03:36 +0800

    debug page index

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [App\Http\Controllers\MoonshotController::class, 'index']);
+Route::get('/', [App\Http\Controllers\DevController::class, 'index']);
