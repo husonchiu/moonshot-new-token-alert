4e877f44e34e20a21f57b785e383996e44d6ba3b
Huson Chiu <cchangive@gmail.com>
2025-01-03 17:32:52 +0800

    gogogogo

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -10,7 +10,7 @@ class WebhookController extends Controller
     public function index($token)
     {
         $updates = Telegram::getWebhookUpdate();
-        if ($updates['chat']['id'] == config('services.telegram-bot-api.chat_id') && isset($updates['message']['reply_to_message']) && $updates['message']['reply_to_message']['message_id'] == config('services.telegram-bot-api.topic_id_token_trade'))
+        if ($updates['message']['chat']['id'] == config('services.telegram-bot-api.chat_id') && isset($updates['message']['reply_to_message']) && $updates['message']['reply_to_message']['message_id'] == config('services.telegram-bot-api.topic_id_token_trade'))
         {
             \Log::info($updates);
         }
